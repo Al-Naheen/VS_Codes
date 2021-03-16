b685d4f9c4b035a3c731c55f83a4759152a4f0dd
Al Naheen <alnaheen543@gmail.com>
2021-03-16 11:47:42 +0600

    discord logos problem has been solved

--- React/Projects(CP)/Discord-Clone/my-app/src/Login.js
+++ React/Projects(CP)/Discord-Clone/my-app/src/Login.js
@@ -14,7 +14,7 @@ function Login() {
         <div className="login">
             
             <div className="login__logo">
-                <img src="https://upload.wikimedia.org/wikipedia/sco/thumb/9/98/Discord_logo.svg/800px-Discord_logo.svg.png" alt="" srcset=""/>
+                <img src="https://logopng.net/wp-content/uploads/2020/07/logo-discord-png-icon-6.png" alt="" srcset=""/>
             </div>
 
             <Button onClick={signIn}>Sign In</Button>
